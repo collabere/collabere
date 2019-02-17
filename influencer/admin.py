e0a54fea9bd726e48248be08e6b15e97e6230e89
Sanskar <snskr95@gmail.com>
2019-02-17 20:00:12 +0530

    registered mappping

--- influencer/admin.py
+++ influencer/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from influencer.models import Influencer
+from influencer.models import Influencer, ClientMapping
 
 admin.site.register(Influencer)
+admin.site.register(ClientMapping)
