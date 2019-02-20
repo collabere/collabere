54b113c8d6e69c077d9b8680367ae2fa9991e87f
Sanskar <snskr95@gmail.com>
2019-02-20 23:54:00 +0530

    addeed egistration view for inf

---
+++ influencer/forms.py
@@ -0,0 +1,19 @@
+
+from django import forms
+
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit
+from .models import Influencer
+
+
+
+class InfluencerForm(forms.ModelForm):
+    class Meta:
+        model = Influencer
+        fields = ('nae', 'email', 'handle', 'dob','gender','city','country')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_method = 'post'
+        self.helper.add_input(Submit('submit', 'Save person'))
