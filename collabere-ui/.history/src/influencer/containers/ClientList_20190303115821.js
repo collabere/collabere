0cf0e21eb4c154ea34992e83c37e78353798ad6f
Sanskar <snskr95@gmail.com>
2019-03-04 22:38:26 +0530

    basic structure layout of ui

---
+++ collabere-ui/.history/src/influencer/containers/ClientList_20190303115821.js
@@ -0,0 +1,39 @@
+import React from "react";
+import axios from "axios";
+import Client from "../components/Client";
+
+
+
+class ClientList extends React.Component {
+  state = {
+    clients: []
+  };
+
+  fetchArticles = () => {
+    axios.get("http://127.0.0.1:8000/influencer/v1/clients/1").then(res => {
+      this.setState({
+        clients: res.data
+      });
+    });
+  }
+
+  componentDidMount() {
+    this.fetchArticles();
+  }
+
+  componentWillReceiveProps(newProps) {
+    if (newProps.token) {
+      this.fetchArticles();      
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <Client data={this.state.clients} /> <br />
+      </div>
+    );
+  }
+}
+
+export default ClientList;
