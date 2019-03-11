fa3db1b6a71ee31d86cd7c75b465c1758492414d
niks2010 <niks.sbg@gmail.com>
2019-03-12 00:36:59 +0530

    homepage ui setup

--- inclyfy/settings.py
+++ inclyfy/settings.py
@@ -42,8 +42,7 @@ INSTALLED_APPS = [
     'influencer',
     'conversations',
     'rest_framework',
-    'crispy_forms',
-    'corsheaders'
+    'crispy_forms'
 ]
 
 CRISPY_TEMPLATE_PACK =  'bootstrap4'
@@ -56,8 +55,7 @@ MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware'
+    'django.middleware.clickjacking.XFrameOptionsMiddleware'
 ]
 
 ROOT_URLCONF = 'inclyfy.urls'
