327b296f36777d911c9efead3897808e7c901639
nappingCommodore <niks.sbg@gmail.com>
2019-04-04 21:05:48 +0530

    webpack configuration is working

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader"
+          }
+        },
+        {
+          test: /\.css$/,
+          use: ["style-loader", "css-loader"]
+        },
+        {
+          test: /\.svg$/,
+          loader: 'svg-inline-loader'
+        }
+      ]
+    }
+  };
