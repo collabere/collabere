fb725b13f8dfbb94221a70b022764ff29cbc83e3
Sanskar <snskr95@gmail.com>
2019-04-27 04:02:28 +0530

    Changed the BrandsSection to class compoenent from functional component

--- frontend/src/homepage/components/BrandSection.js
+++ frontend/src/homepage/components/BrandSection.js
@@ -1,16 +1,47 @@
 import React from "react";
-import { Input } from 'antd';
+import { Input, Button } from "antd";
+import axios from "axios";
 
-export default function BrandSection({ dark, id }) {
+
+class BrandSection extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: ''
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleEmailSubmit = this.handleEmailSubmit.bind(this);
+
+  }
+  
+
+handleChange(event) {
+console.log("impur change")
+this.setState({email: event.target.value});
+};
+
+
+handleEmailSubmit () {
+  console.log("impur change ready")
+  axios.post('http://127.0.0.1:8000/client/intro_email', {
+    email: this.state.email
+  })
+}
+
+render() {
+
   return (
-    <div className={"section" + (dark ? " section-dark" : "")}>
-      <div className="section-content" id={id}>
-      <h1>Product for brands
-</h1>
-        <p>Drop your email here to know
-</p>
-<Input placeholder="Enter email here" />
+    <div>
+      <div className="section-content" id={this.props.id}>
+        <h1>Product for brands</h1>
+        <p>Drop your email here to know</p>
+        <Input onChange = {this.handleChange} placeholder="Enter email here" />
+        <Button onClick = {this.handleEmailSubmit}>Submit</Button>
       </div>
     </div>
   );
 }
+}
+export default BrandSection;
+
+
