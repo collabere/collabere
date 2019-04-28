cd73d028a8a5436f324746f7657ba686112e95ab
Sanskar <snskr95@gmail.com>
2019-04-29 00:12:41 +0530

    included all fields in registration form

--- frontend/src/register/registerModal.js
+++ frontend/src/register/registerModal.js
@@ -1,87 +1,162 @@
 import React from "react";
-import { ModalBody, Col, Form, FormGroup, Label, Input, Button, } from 'reactstrap';
+import {
+  ModalBody,
+  Col,
+  Form,
+  FormGroup,
+  Label,
+  Input,
+  Button
+} from "reactstrap";
+import { Modal } from "antd";
+import axios from "axios";
 
 class RegisterModal extends React.Component {
-    constructor(props) {
-        super(props);
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      firstName: "",
+      lastName: "",
+      email: "",
+      handle: "",
+      dob: "",
+      gender: "",
+      city: "",
+      country: "",
+      industry: "",
+      password: ""
+    };
+  }
 
-    render() {
-        return (
-            <Form className="form">
-                <Col>
-                    <FormGroup>
-                        <Label>First Name</Label>
-                        <Input
-                            type="text"
-                            name="firstName"
-                            id="firstName"
-                            placeholder="John"
-                        />
-                    </FormGroup>
-                </Col>
-                <Col>
-                    <FormGroup>
-                        <Label>Last Name</Label>
-                        <Input
-                            type="text"
-                            name="lastName"
-                            id="lastName"
-                            placeholder="Doe"
-                        />
-                    </FormGroup>
-                </Col>
-                <Col>
-                    <FormGroup>
-                        <Label>User Name</Label>
-                        <Input
-                            type="text"
-                            name="userName"
-                            id="userName"
-                            placeholder="johnDoe420"
-                        />
-                    </FormGroup>
-                </Col>
-                <Col>
-                    <FormGroup>
-                        <Label>Email</Label>
-                        <Input
-                            type="email"
-                            name="email"
-                            id="email"
-                            placeholder="johnDoe@gmail.com"
-                        />
-                    </FormGroup>
-                </Col>
-                <Col>
-                    <FormGroup>
-                        <Label for="examplePassword">Password</Label>
-                        <Input
-                            type="password"
-                            name="password"
-                            id="examplePassword1"
-                            placeholder="********"
-                        />
-                    </FormGroup>
-                </Col>
-                <Col>
-                    <FormGroup>
-                        <Label for="examplePassword">Re-enter Password</Label>
-                        <Input
-                            type="password"
-                            name="password"
-                            id="examplePassword2"
-                            placeholder="********"
-                        />
-                    </FormGroup>
-                </Col>
-                <Col>
-                    <Button color="primary">Submit</Button>{' '}
-                    <Button color="secondary">Cancel</Button>
-                </Col>
-            </Form>
-        );
-    }
+  handleCredentialSubmit() {
+    axios({
+      method: "post",
+      url: "http://127.0.0.1:8000/influencer/login/",
+      data: {
+        name: this.state.firstName.concat(this.state.lastName),
+        email: this.state.email,
+        handle: this.state.handle,
+        dob: this.state.dob,
+        gender: this.state.gender,
+        city: this.state.city,
+        country: this.state.country,
+        industry: this.state.industry,
+        password: this.state.password
+      },
+      headers: {
+        "content-type": "application/json"
+      }
+    })
+      .then(function(response) {
+        console.log(response);
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+  }
+
+  render() {
+    return (
+      <div style={{ maxWidth: "700px" }}>
+        <Form className="form">
+          <Col>
+            <FormGroup>
+              <Label>First Name</Label>
+              <Input type="text" name="firstName" id="firstName" />
+            </FormGroup>
+          </Col>
+          <Col>
+            <FormGroup>
+              <Label>Last Name</Label>
+              <Input type="text" name="lastName" id="lastName" />
+            </FormGroup>
+          </Col>
+          <Col>
+            <FormGroup>
+              <Label>Gender</Label>
+              <Input type="select" name="select" id="exampleSelect">
+                <option>Male</option>
+                <option>Female</option>
+                <option>Would Not Like To Reveal</option>
+              </Input>
+            </FormGroup>
+          </Col>
+          <Col>
+            <FormGroup>
+              <Label>Industry</Label>
+              <Input type="select" name="select" id="exampleSelect">
+                <option>Fashion</option>
+                <option>Photography</option>
+                <option>Food</option>
+                <option>Travel</option>
+                <option>Blogging</option>
+                <option>Vlogging</option>
+                <option>Poetry</option>
+                <option>Others</option>
+              </Input>
+            </FormGroup>
+          </Col>
+          <Col>
+            <FormGroup>
+              <Label>Email</Label>
+              <Input type="email" name="email" id="email" />
+            </FormGroup>
+          </Col>
+          <Col>
+            <FormGroup>
+              <Label for="exampleDate">Date</Label>
+              <Input
+                type="date"
+                name="date"
+                id="exampleDate"
+                placeholder="date placeholder"
+              />
+            </FormGroup>
+          </Col>
+          <Col>
+            <FormGroup>
+              <Label>City</Label>
+              <Input type="text" name="firstName" id="firstName" />
+            </FormGroup>
+          </Col>
+          <Col>
+            <FormGroup>
+              <Label>Country</Label>
+              <Input type="text" name="firstName" id="firstName" />
+            </FormGroup>
+          </Col>
+          <Col>
+            <FormGroup>
+              <Label for="examplePassword">Password</Label>
+              <Input
+                type="password"
+                name="password"
+                id="examplePassword1"
+                placeholder="********"
+              />
+            </FormGroup>
+          </Col>
+          <Col>
+            <FormGroup>
+              <Label for="examplePassword">Re-enter Password</Label>
+              <Input
+                type="password"
+                name="password"
+                id="examplePassword2"
+                placeholder="********"
+              />
+            </FormGroup>
+          </Col>
+          <Col>
+            <Button color="primary" onClick={this.handleSubmit}>
+              Submit
+            </Button>
+            <Button color="secondary">Cancel</Button>
+          </Col>
+        </Form>
+      </div>
+    );
+  }
 }
 
 export default RegisterModal;
