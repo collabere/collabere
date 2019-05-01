cfee5f54d0446a231dcfd1d220709cee9e891f73
nappingCommodore <niks.sbg@gmail.com>
2019-05-01 19:47:24 +0530

    changed index.html file location

--- frontend/views.py
+++ frontend/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'static/index.html')
+    return render(request, 'static/frontend/index.html')
 
 def home(request):
     print(request)
