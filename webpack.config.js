e5096446ca7c65a3040106e14477b84f5445ea6f
nappingCommodore <niks.sbg@gmail.com>
2019-05-01 21:25:05 +0530

    webpack config change to reduce size of generated bundle

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,8 @@
+
+var webpack = require('webpack');
+var CompressionPlugin = require('compression-webpack-plugin');
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+
 module.exports = {
   watch: true,
   module: {
@@ -31,5 +36,30 @@ module.exports = {
         ],
       }
     ]
-  }
+  },
+  plugins: [
+    new webpack.DefinePlugin({ //<--key to reduce React's size
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+    // new webpack.optimize.DedupePlugin(),
+    new UglifyJsPlugin({
+      uglifyOptions: {
+      warnings: false,
+      ie8: false,
+      output: {
+      comments: false
+      }
+      }
+      }),
+    new webpack.optimize.AggressiveMergingPlugin(),
+    new CompressionPlugin({
+      filename: "[path].gz[query]",
+      algorithm: "gzip",
+      test: /\.js$|\.css$|\.html$/,
+      threshold: 10240,
+      minRatio: 0.8
+    })
+  ],
 };
