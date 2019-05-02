e0e5235a051a1a395998b354584146841f7a4e3c
Sanskar <snskr95@gmail.com>
2019-05-02 23:39:21 +0530

    added registraion from without backend intgration

--- frontend/src/register/RegisterModal.js
+++ frontend/src/register/RegisterModal.js
@@ -1,24 +1,26 @@
 import React from "react";
 import { Link as _Link } from 'react-router-dom';
 
+
 import {
-  ModalBody,
   Col,
   Form,
   FormGroup,
   Label,
   Input,
-  Button
+  Button,
+  FormFeedback,
+  Modal, ModalHeader, ModalBody, ModalFooter
 } from "reactstrap";
-import { Modal } from "antd";
 import axios from "axios";
+import LoginScreen from "../login/LoginScreen";
+import LoginModal from "../login/loginModal";
 
 class RegisterModal extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      firstName: "",
-      lastName: "",
+      name: '',
       email: "",
       handle: "",
       dob: "",
@@ -26,16 +28,27 @@ class RegisterModal extends React.Component {
       city: "",
       country: "",
       industry: "",
-      password: ""
+      passwordFirst: '',
+      passwordSecond: '',
+      successModal: false,
+      loginScreenOpen: false
     };
+    this.handleChangeOfInputFields = this.handleChangeOfInputFields.bind(this);
+    this.handleChangeOfInputName = this.handleChangeOfInputName.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.toggleModal = this.toggleModal.bind(this);
+    this.openLoginModal = this.openLoginModal.bind(this);
+    this.closeLoginModal = this.closeLoginModal.bind(this);
+
+
   }
 
   handleCredentialSubmit() {
     axios({
       method: "post",
-      url: "http://127.0.0.1:8000/influencer/login/",
+      url: "http://127.0.0.1:8000/influencer/register/",
       data: {
-        name: this.state.firstName.concat(this.state.lastName),
+        name: this.state.name,
         email: this.state.email,
         handle: this.state.handle,
         dob: this.state.dob,
@@ -57,36 +70,59 @@ class RegisterModal extends React.Component {
       });
   }
 
+  handleChangeOfInputFields (event) {
+    event.preventDefault()
+    this.setState({ [event.target.name]: event.target.value });
+  }
+
+  handleChangeOfInputName (event) {
+    event.preventDefault()
+    this.setState({ [event.target.name]: event.target.value });
+  }
+
+  toggleModal() {
+    this.setState({successModal: false});
+  }
+
+  handleSubmit(){
+    this.setState({
+      successModal: true
+    });
+  }
+
+  openLoginModal(){
+    this.setState({loginScreenOpen: true})
+  }
+
+  closeLoginModal() {
+    this.setState({loginScreenOpen: false})
+  }
+
   render() {
     return (
-      <div style={{ maxWidth: "700px" }}>
+      <div style={{ maxWidth: "700px" , maxHeight: "500px"}}>
         <Form className="form">
           <Col>
             <FormGroup>
-              <Label>First Name</Label>
-              <Input type="text" name="firstName" id="firstName" />
-            </FormGroup>
-          </Col>
-          <Col>
-            <FormGroup>
-              <Label>Last Name</Label>
-              <Input type="text" name="lastName" id="lastName" />
+              <Label>Name</Label>
+              <Input type="text" name="name" id="name"  onChange={this.handleChangeOfInputName} />
             </FormGroup>
           </Col>
           <Col>
             <FormGroup>
               <Label>Gender</Label>
-              <Input type="select" name="select" id="exampleSelect">
+              <Input type="select" name="gender" id="exampleSelect"  onChange={this.handleChangeOfInputFields}>
                 <option>Male</option>
                 <option>Female</option>
                 <option>Would Not Like To Reveal</option>
+
               </Input>
             </FormGroup>
           </Col>
           <Col>
             <FormGroup>
               <Label>Industry</Label>
-              <Input type="select" name="select" id="exampleSelect">
+              <Input type="select" name="industry" id="exampleSelect" onChange={this.handleChangeOfInputFields}>
                 <option>Fashion</option>
                 <option>Photography</option>
                 <option>Food</option>
@@ -101,7 +137,7 @@ class RegisterModal extends React.Component {
           <Col>
             <FormGroup>
               <Label>Email</Label>
-              <Input type="email" name="email" id="email" />
+              <Input type="email" name="email" id="email" onChange={this.handleChangeOfInputFields} />
             </FormGroup>
           </Col>
           <Col>
@@ -109,22 +145,23 @@ class RegisterModal extends React.Component {
               <Label for="exampleDate">Date</Label>
               <Input
                 type="date"
-                name="date"
+                name="dob"
                 id="exampleDate"
                 placeholder="date placeholder"
+                onChange={this.handleChangeOfInputName}
               />
             </FormGroup>
           </Col>
           <Col>
             <FormGroup>
               <Label>City</Label>
-              <Input type="text" name="firstName" id="firstName" />
+              <Input type="text" name="city" id="firstName" onChange={this.handleChangeOfInputFields} />
             </FormGroup>
           </Col>
           <Col>
             <FormGroup>
               <Label>Country</Label>
-              <Input type="text" name="firstName" id="firstName" />
+              <Input type="text" name="country" id="firstName" onChange={this.handleChangeOfInputFields}/>
             </FormGroup>
           </Col>
           <Col>
@@ -132,9 +169,10 @@ class RegisterModal extends React.Component {
               <Label for="examplePassword">Password</Label>
               <Input
                 type="password"
-                name="password"
+                name="passwordFirst"
                 id="examplePassword1"
                 placeholder="********"
+                onChange={this.handleChangeOfInputFields}
               />
             </FormGroup>
           </Col>
@@ -143,10 +181,14 @@ class RegisterModal extends React.Component {
               <Label for="examplePassword">Re-enter Password</Label>
               <Input
                 type="password"
-                name="password"
+                name="passwordSecond"
                 id="examplePassword2"
                 placeholder="********"
+                onChange={this.handleChangeOfInputFields}
+                valid={(this.state.passwordFirst === this.state.passwordSecond) && this.state.passwordFirst !== ''}
+                invalid={this.state.passwordSecond !== this.state.passwordFirst}
               />
+              <FormFeedback invalid>Passwords do not match</FormFeedback>
             </FormGroup>
           </Col>
           <Col>
@@ -156,6 +198,27 @@ class RegisterModal extends React.Component {
             <_Link to ="/"> <Button color="secondary">Cancel</Button></_Link>
           </Col>
         </Form>
+        <Modal isOpen={this.state.successModal}>
+          <ModalHeader>Success!</ModalHeader>
+          <ModalBody>
+            Yayy! You have registered successfully
+          </ModalBody>
+          <ModalFooter>
+          <Button color="primary" onClick={this.openLoginModal}>Take me to Login!</Button>
+            <Button color="secondary" onClick={this.toggleModal}>Cancel</Button>
+          </ModalFooter>
+        </Modal>
+        <Modal isOpen={this.state.loginScreenOpen} >
+          <ModalHeader>Login</ModalHeader>
+          <ModalBody>
+          {this.state.loginScreenOpen ? <LoginModal /> : null}
+          </ModalBody>
+          <ModalFooter>
+          <Button color="primary" onClick={this.toggleLoginScreen}>Login</Button>
+            <Button color="secondary" onClick={this.closeLoginModal}>Cancel</Button>
+          </ModalFooter>
+        </Modal>
+
       </div>
     );
   }
