d65aa29abd4d33025a2f8016ead34421a0486ad9
Sanskar <snskr95@gmail.com>
2019-05-05 16:20:02 +0530

    added name validation integrated with backend

--- frontend/src/register/RegisterModal.js
+++ frontend/src/register/RegisterModal.js
@@ -1,6 +1,5 @@
 import React from "react";
-import { Link as _Link } from 'react-router-dom';
-
+import { Link as _Link } from "react-router-dom";
 
 import {
   Col,
@@ -10,7 +9,10 @@ import {
   Input,
   Button,
   FormFeedback,
-  Modal, ModalHeader, ModalBody, ModalFooter
+  Modal,
+  ModalHeader,
+  ModalBody,
+  ModalFooter
 } from "reactstrap";
 import axios from "axios";
 import LoginScreen from "../login/LoginScreen";
@@ -19,7 +21,7 @@ import LoginModal from "../login/loginModal";
 function checkPasswordComplexity(pwd) {
   var regularExpression = /^(?=.*[a-zA-Z])(?=.*[0-9]).+$/;
   var valid = regularExpression.test(pwd);
-  console.log(pwd,valid)
+  console.log(pwd, valid);
   return valid;
 }
 
@@ -27,7 +29,7 @@ class RegisterModal extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      name: '',
+      name: "",
       email: "",
       handle: "",
       dob: "",
@@ -35,10 +37,11 @@ class RegisterModal extends React.Component {
       city: "",
       country: "",
       industry: "",
-      passwordFirst: '',
-      passwordSecond: '',
+      passwordFirst: "",
+      passwordSecond: "",
       successModal: false,
-      loginScreenOpen: false
+      loginScreenOpen: false,
+      usernameValidFlag: ""
     };
     this.handleChangeOfInputFields = this.handleChangeOfInputFields.bind(this);
     this.handleChangeOfInputName = this.handleChangeOfInputName.bind(this);
@@ -46,11 +49,8 @@ class RegisterModal extends React.Component {
     this.toggleModal = this.toggleModal.bind(this);
     this.openLoginModal = this.openLoginModal.bind(this);
     this.closeLoginModal = this.closeLoginModal.bind(this);
-
-
+    this.validateUsername = this.validateUsername.bind(this);
   }
-  //make height less
-  //border lagana hai
 
   handleCredentialSubmit() {
     axios({
@@ -79,59 +79,106 @@ class RegisterModal extends React.Component {
       });
   }
 
-  handleChangeOfInputFields (event) {
-    event.preventDefault()
+  validateUsername(name) {
+    axios
+      .get("http://127.0.0.1:8000/influencer/username", {
+        params: { username: name }
+      })
+      .then(res => {
+        console.log(res.data);
+        this.setState({
+          usernameValidFlag: res.data
+        });
+      });
+  }
+
+  handleChangeOfInputFields(event) {
+    event.preventDefault();
     this.setState({ [event.target.name]: event.target.value });
   }
 
-  handleChangeOfInputName (event) {
-    event.preventDefault()
+  handleChangeOfInputName(event) {
+    event.preventDefault();
     this.setState({ [event.target.name]: event.target.value });
+    this.validateUsername(event.target.value);
   }
 
   toggleModal() {
-    this.setState({successModal: false});
+    this.setState({ successModal: false });
   }
 
-  handleSubmit(){
+  handleSubmit() {
     this.setState({
       successModal: true
     });
   }
 
-  openLoginModal(){
-    this.setState({loginScreenOpen: true})
+  openLoginModal() {
+    this.setState({ loginScreenOpen: true });
   }
 
   closeLoginModal() {
-    this.setState({loginScreenOpen: false})
+    this.setState({ loginScreenOpen: false });
   }
-note
+  note;
   render() {
     return (
-      <div style={{ maxWidth: "700px", marginLeft: "600px", border: '1px solid green', padding: '2px', marginTop: '40px'}}>
-        <Form className="form" style ={{marginTop: '20px', marginBottom: '20px'}}>
+      <div
+        style={{
+          maxWidth: "700px",
+          marginLeft: "350px",
+          border: "1px solid green",
+          padding: "2px",
+          marginTop: "40px"
+        }}
+      >
+        <Form
+          className="form"
+          style={{ marginTop: "20px", marginBottom: "20px" }}
+        >
           <Col>
             <FormGroup>
               <Label>Name</Label>
-              <Input type="text" name="name" id="name"  onChange={this.handleChangeOfInputName} />
+              <Input
+                type="text"
+                name="name"
+                id="name"
+                onChange={this.handleChangeOfInputName}
+                valid={!this.state.usernameValidFlag && this.state.name !== ''}
+                invalid={this.state.usernameValidFlag}
+              />
+              <FormFeedback invalid>
+               The user name {this.state.name} is already registered ,please use another one.
+              </FormFeedback>
+              <FormFeedback valid>
+               Yayyy! the username {this.state.name} is available.
+              </FormFeedback>
             </FormGroup>
           </Col>
           <Col>
             <FormGroup>
               <Label>Gender</Label>
-              <Input type="select" name="gender" id="exampleSelect"  onChange={this.handleChangeOfInputFields}>
+              <Input
+                type="select"
+                name="gender"
+                id="exampleSelect"
+                onChange={this.handleChangeOfInputFields}
+              >
                 <option>Male</option>
                 <option>Female</option>
                 <option>Would Not Like To Reveal</option>
-
               </Input>
             </FormGroup>
           </Col>
           <Col>
             <FormGroup>
               <Label>Industry</Label>
-              <Input type="select" name="industry" id="exampleSelect" onChange={this.handleChangeOfInputFields}>
+              <Input
+                type="select"
+                name="industry"
+                id="exampleSelect"
+                onChange={this.handleChangeOfInputFields}
+              >
                 <option>Fashion</option>
                 <option>Photography</option>
                 <option>Food</option>
@@ -146,7 +193,12 @@ note
           <Col>
             <FormGroup>
               <Label>Email</Label>
-              <Input type="email" name="email" id="email" onChange={this.handleChangeOfInputFields} />
+              <Input
+                type="email"
+                name="email"
+                id="email"
+                onChange={this.handleChangeOfInputFields}
+              />
             </FormGroup>
           </Col>
           <Col>
@@ -164,13 +216,23 @@ note
           <Col>
             <FormGroup>
               <Label>City</Label>
-              <Input type="text" name="city" id="firstName" onChange={this.handleChangeOfInputFields} />
+              <Input
+                type="text"
+                name="city"
+                id="firstName"
+                onChange={this.handleChangeOfInputFields}
+              />
             </FormGroup>
           </Col>
           <Col>
             <FormGroup>
               <Label>Country</Label>
-              <Input type="text" name="country" id="firstName" onChange={this.handleChangeOfInputFields}/>
+              <Input
+                type="text"
+                name="country"
+                id="firstName"
+                onChange={this.handleChangeOfInputFields}
+              />
             </FormGroup>
           </Col>
           <Col>
@@ -181,11 +243,17 @@ note
                 name="passwordFirst"
                 id="examplePassword1"
                 placeholder="********"
-                valid = {checkPasswordComplexity(this.state.passwordFirst)}
-                invalid = {!checkPasswordComplexity(this.state.passwordFirst) && this.state.passwordFirst !== ''}
+                valid={checkPasswordComplexity(this.state.passwordFirst)}
+                invalid={
+                  !checkPasswordComplexity(this.state.passwordFirst) &&
+                  this.state.passwordFirst !== ""
+                }
                 onChange={this.handleChangeOfInputFields}
               />
-              <FormFeedback invalid>Password should contain atleast each of character, number and symbol</FormFeedback>
+              <FormFeedback invalid>
+                Password should contain atleast each of character, number and
+                symbol
+              </FormFeedback>
             </FormGroup>
           </Col>
           <Col>
@@ -197,7 +265,10 @@ note
                 id="examplePassword2"
                 placeholder="********"
                 onChange={this.handleChangeOfInputFields}
-                valid={(this.state.passwordFirst === this.state.passwordSecond) && this.state.passwordFirst !== ''}
+                valid={
+                  this.state.passwordFirst === this.state.passwordSecond &&
+                  this.state.passwordFirst !== ""
+                }
                 invalid={this.state.passwordSecond !== this.state.passwordFirst}
               />
               <FormFeedback invalid>Passwords do not match</FormFeedback>
@@ -207,30 +278,38 @@ note
             <Button color="primary" onClick={this.handleSubmit}>
               Submit
             </Button>
-            <_Link to ="/"> <Button color="secondary">Cancel</Button></_Link>
+            <_Link to="/">
+              {" "}
+              <Button color="secondary">Cancel</Button>
+            </_Link>
           </Col>
         </Form>
         <Modal isOpen={this.state.successModal}>
           <ModalHeader>Success!</ModalHeader>
-          <ModalBody>
-            Yayy! You have registered successfully
-          </ModalBody>
+          <ModalBody>Yayy! You have registered successfully</ModalBody>
           <ModalFooter>
-          <Button color="primary" onClick={this.openLoginModal}>Take me to Login!</Button>
-            <Button color="secondary" onClick={this.toggleModal}>Cancel</Button>
+            <Button color="primary" onClick={this.openLoginModal}>
+              Take me to Login!
+            </Button>
+            <Button color="secondary" onClick={this.toggleModal}>
+              Cancel
+            </Button>
           </ModalFooter>
         </Modal>
-        <Modal isOpen={this.state.loginScreenOpen} >
+        <Modal isOpen={this.state.loginScreenOpen}>
           <ModalHeader>Login</ModalHeader>
           <ModalBody>
-          {this.state.loginScreenOpen ? <LoginModal /> : null}
+            {this.state.loginScreenOpen ? <LoginModal /> : null}
           </ModalBody>
           <ModalFooter>
-          <Button color="primary" onClick={this.toggleLoginScreen}>Login</Button>
-            <Button color="secondary" onClick={this.closeLoginModal}>Cancel</Button>
+            <Button color="primary" onClick={this.toggleLoginScreen}>
+              Login
+            </Button>
+            <Button color="secondary" onClick={this.closeLoginModal}>
+              Cancel
+            </Button>
           </ModalFooter>
         </Modal>
-
       </div>
     );
   }
