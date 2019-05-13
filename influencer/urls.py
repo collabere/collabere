a3ef3fbc322d07959221e7cba8dd0a978bdf5e71
Sanskar <snskr95@gmail.com>
2019-05-13 23:38:26 +0530

    added some urls

--- influencer/urls.py
+++ influencer/urls.py
@@ -4,16 +4,15 @@ from django.conf.urls import url
 from django.contrib.auth import views
 from django.views.decorators.csrf import csrf_exempt
 
-from .views import home, handleLogin, getInfluencerFromInfluencerUsername, getClientsBasedOnInfluencers, getInfluencerDetails, deleteInfluencer, putInfluencer,handleRegisterInfluencer, usernameFetch
-from django.urls import reverse_lazy
+from .views import handleLogin, getInfluencerFromInfluencerUsername, getClientsBasedOnInfluencers, getInfluencerDetails, deleteInfluencer, putInfluencer,handleRegisterInfluencer, usernameFetch
+
 
 urlpatterns = [
     url(r'login/', handleLogin, name='influencer_login'),
-    url(r'home/',home,name='influencer_home'),
     url(r'logout/',views.LogoutView.as_view(),name='influencer_logut'),
-    url(r'v1/(\d+)$', getInfluencerDetails, name='influencer_details'),
-    url(r'v1/clients/(\d+)$', getClientsBasedOnInfluencers, name='influencer_clients'),
+    url(r'v1/clients', getClientsBasedOnInfluencers, name='influencer_clients'),
     url(r'username', usernameFetch, name='influence_username_fetch'),
+    url(r'user_details', getInfluencerFromInfluencerUsername, name='influencer_details'),
     url(r'delete/(\d+)$', deleteInfluencer, name='influencer_delete'),
     url(r'put$', putInfluencer, name='insert_influencer'),
     url(r'register/', handleRegisterInfluencer, name='influencer_register'),
