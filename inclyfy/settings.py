9d7af7fc1aff06095c4c8e525c9b6582123f3694
Sanskar <snskr95@gmail.com>
2019-08-25 19:36:31 +0530

    changed the db config to sql lite

--- inclyfy/settings.py
+++ inclyfy/settings.py
@@ -106,16 +106,25 @@ WSGI_APPLICATION = 'inclyfy.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
+#Will be used later ,now sql lite will be used
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'collabere_db',
+#         'USER': 'postgres',
+#         'PASSWORD': 'warangal2017',
+#         'HOST': 'collabere-db.cz2o54urftre.ap-south-1.rds.amazonaws.com',
+#         'PORT': '5432',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'collabere_db',
-        'USER': 'postgres',
-        'PASSWORD': 'warangal2017',
-        'HOST': 'collabere-db.cz2o54urftre.ap-south-1.rds.amazonaws.com',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
