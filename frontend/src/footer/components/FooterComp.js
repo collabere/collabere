acfa2272985fb89b1e952de1bb0a3834791987aa
Sanskar <snskr95@gmail.com>
2019-09-10 19:18:34 +0530

    included privacy policy

--- frontend/src/footer/components/FooterComp.js
+++ frontend/src/footer/components/FooterComp.js
@@ -1,28 +1,25 @@
 import React from "react";
-import { Button } from "antd";
+import Button from '@material-ui/core/Button';
 import { Link } from "react-router-dom";
 
 export default class FooterComp extends React.Component {
   render() {
     return (
-      <div style={{ height: "150px" }}>
+      <div  style={{margin: "auto" }}>
         <nav
           class="navbar navbar-light"
-          style={{ backgroundColor: "#464646", height: "150px" }}
+          style={{ backgroundColor: "#696969", height: "150px" }}
         >
-          <form class="form-inline" style={{  paddingLeft: "170px" }}
+          <form inline
         >
             <div>
-              <Link to='/terms-conditions'><Button style ={{fontSize: '18px' }}>Terms and conditions</Button></Link>
+              <Link  style={{textDecoration: 'none'}} to='/terms-conditions'><Button style ={{fontSize: '18px' }}>Terms and conditions</Button></Link>
             </div>
-            <div  style={{ paddingLeft: "150px" }}>
-            <Link to='/privacy-policy'> <Button style ={{fontSize: '18px' }}>Privacy policy</Button></Link>
+            <div  >
+            <Link  style={{textDecoration: 'none'}} to='/privacy-policy'> <Button style ={{fontSize: '18px' }}>Privacy policy</Button></Link>
             </div>
-            <div  style={{ paddingLeft: "150px" }}>
-              <Button style ={{fontSize: '18px' }}>Careers</Button>
-            </div>
-            <div  style={{ paddingLeft: "150px" }}>
-            <Link to='/pricing'> <Button style ={{fontSize: '18px' }}>Pricing</Button></Link>
+            <div  >
+            <Link style={{textDecoration: 'none'}} to='/pricing'> <Button style ={{fontSize: '18px' }}>Pricing</Button></Link>
             </div>
           </form>
         </nav>
