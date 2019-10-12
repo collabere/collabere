f7a9f2587d7375a4fafa62607ac2c2cb0b3d2fe8
nappingCommodore <niks.sbg@gmail.com>
2019-10-12 23:17:36 +0530

    using sqlite db

--- inclyfy/settings.py
+++ inclyfy/settings.py
@@ -220,14 +220,14 @@ WSGI_APPLICATION = 'inclyfy.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'inclyfy',#'collabere_db',
-        'USER': 'postgres',
-        'PASSWORD': 'warangal2017',
-        'HOST': 'localhost',#'collabere-db.cz2o54urftre.ap-south-1.rds.amazonaws.com',
-        'PORT': '5432',
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        # 'NAME': 'inclyfy',#'collabere_db',
+        # 'USER': 'postgres',
+        # 'PASSWORD': 'warangal2017',
+        # 'HOST': 'localhost',#'collabere-db.cz2o54urftre.ap-south-1.rds.amazonaws.com',
+        # 'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
