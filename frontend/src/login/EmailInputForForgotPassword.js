c049b85470a920edcf0b3db1a0823b8256b34d29
Sanskar <snskr95@gmail.com>
2019-11-24 03:03:56 +0530

    added nav bar in forgot and change password compoenents

--- frontend/src/login/EmailInputForForgotPassword.js
+++ frontend/src/login/EmailInputForForgotPassword.js
@@ -1,34 +1,29 @@
 import React from "react";
-import {
-  Col,
-  Form,
-  FormGroup,
-  Label,
-  Input,
-} from "reactstrap";
+import { Col, Form, FormGroup, Label, Input } from "reactstrap";
 import * as MaterialUiLibrary from "@material-ui/core";
 
-import { Link, Redirect } from "react-router-dom";
 import axios from "axios";
 import { Typography } from "antd";
-import Dialog from '@material-ui/core/Dialog';
-import DialogActions from '@material-ui/core/DialogActions';
-import DialogContent from '@material-ui/core/DialogContent';
-import DialogContentText from '@material-ui/core/DialogContentText';
-import DialogTitle from '@material-ui/core/DialogTitle';
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import CardContent from "@material-ui/core/CardContent";
+import Card from "@material-ui/core/Card";
+import HomeNavBar from "../homepage/components/Navbar";
 
 class EmailInputForReset extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-        email:"",
-        successModalOpen: false
+      email: "",
+      successModalOpen: false
     };
     this.handleChangeOfInputFields = this.handleChangeOfInputFields.bind(this);
     this.handleSend = this.handleSend.bind(this);
-    this.handleSuccessModalOpen= this.handleSuccessModalOpen.bind(this);
-    this.handleSuccessModalClose= this.handleSuccessModalClose.bind(this);
-
+    this.handleSuccessModalOpen = this.handleSuccessModalOpen.bind(this);
+    this.handleSuccessModalClose = this.handleSuccessModalClose.bind(this);
   }
 
   handleSend() {
@@ -36,24 +31,23 @@ class EmailInputForReset extends React.Component {
       method: "put",
       url: `/influencer/auth/send_email_to_reset_password/`,
       data: {
-        influencerEmail: this.state.email,
+        influencerEmail: this.state.email
       },
       headers: {
         "content-type": "application/json"
       }
     }).then(response => {
-      this.handleSuccessModalOpen()
+      this.handleSuccessModalOpen();
     });
   }
 
-  handleSuccessModalOpen(){
-      this.setState({successModalOpen: true})
+  handleSuccessModalOpen() {
+    this.setState({ successModalOpen: true });
   }
 
-
-  handleSuccessModalClose(){
-    this.setState({successModalOpen: false})
-}
+  handleSuccessModalClose() {
+    this.setState({ successModalOpen: false });
+  }
 
   handleChangeOfInputFields(event) {
     event.preventDefault();
@@ -62,46 +56,67 @@ class EmailInputForReset extends React.Component {
 
   render() {
     return (
-      <div  style={{
-        maxWidth: "70%",
-        border: "1px solid green",
-        padding: "2px",
-        margin: "5% auto 5% auto",
-      }}>
-      <Form className="form">
-        <Col>
-          <FormGroup>
-            <Label for="examplePassword">Enter email</Label>
-            <Input
-              type="email"
-              name="email"
-              id="email"
-              onChange={this.handleChangeOfInputFields}
-            />
-            <Typography>We will send you a password reset link on the above email</Typography>
-          </FormGroup>
-        </Col>
-        <MaterialUiLibrary.Button variant="contained" color="primary" onClick={this.handleSend}>Send Link</MaterialUiLibrary.Button>
-
-      </Form>
-      <Dialog
-        open={this.state.successModalOpen}
-        onClose={this.handleSuccessModalClose}
-        aria-labelledby="alert-dialog-title"
-        aria-describedby="alert-dialog-description"
+      <div
+        style={{
+          maxWidth: "70%",
+          padding: "2px",
+          margin: "5% auto 5% auto"
+        }}
       >
-        <DialogTitle id="alert-dialog-title">{"Email Sent Successfully"}</DialogTitle>
-        <DialogContent>
-          <DialogContentText id="alert-dialog-description">
-            Please check your mail and click on the link sent to reset the password.
-          </DialogContentText>
-        </DialogContent>
-        <DialogActions>
-          <MaterialUiLibrary.Button onClick={this.handleSuccessModalClose} color="primary">
-            OK
-          </MaterialUiLibrary.Button>
-        </DialogActions>
-      </Dialog>
+        <HomeNavBar />
+
+        <Card style={{ backgroundColor: "#D8BFD8", marginTop: '2rem' }}>
+          <CardContent>
+            <Form className="form">
+              <Col>
+                <FormGroup>
+                  <Label for="examplePassword">Enter Email</Label>
+                  <Input
+                    type="email"
+                    name="email"
+                    id="email"
+                    onChange={this.handleChangeOfInputFields}
+                  />
+                  <Typography>
+                    We will send you a password reset link on the above email
+                  </Typography>
+                </FormGroup>
+              </Col>
+              <MaterialUiLibrary.Button
+                variant="contained"
+                color="primary"
+                onClick={this.handleSend}
+              >
+                Send Link
+              </MaterialUiLibrary.Button>
+            </Form>
+          </CardContent>
+        </Card>
+
+        <Dialog
+          open={this.state.successModalOpen}
+          onClose={this.handleSuccessModalClose}
+          aria-labelledby="alert-dialog-title"
+          aria-describedby="alert-dialog-description"
+        >
+          <DialogTitle id="alert-dialog-title">
+            {"Email Sent Successfully"}
+          </DialogTitle>
+          <DialogContent>
+            <DialogContentText id="alert-dialog-description">
+              Please check your mail and click on the link sent to reset the
+              password.
+            </DialogContentText>
+          </DialogContent>
+          <DialogActions>
+            <MaterialUiLibrary.Button
+              onClick={this.handleSuccessModalClose}
+              color="primary"
+            >
+              OK
+            </MaterialUiLibrary.Button>
+          </DialogActions>
+        </Dialog>
       </div>
     );
   }
--- frontend/src/login/ForgotPasswordComponent.js
+++ frontend/src/login/ForgotPasswordComponent.js
@@ -1,33 +1,28 @@
 import React from "react";
-import {
-  Col,
-  Form,
-  FormGroup,
-  Label,
-  Input,
-} from "reactstrap";
+import { Col, Form, FormGroup, Label, Input } from "reactstrap";
 import * as MaterialUiLibrary from "@material-ui/core";
 
-import { Link, Redirect } from "react-router-dom";
 import axios from "axios";
-import Dialog from '@material-ui/core/Dialog';
-import DialogActions from '@material-ui/core/DialogActions';
-import DialogContent from '@material-ui/core/DialogContent';
-import DialogContentText from '@material-ui/core/DialogContentText';
-import DialogTitle from '@material-ui/core/DialogTitle';
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import HomeNavBar from "../homepage/components/Navbar";
 
 class EmailInputForReset extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-        password:"",
-        successModalOpen: false
+      password: "",
+      successModalOpen: false
     };
     this.handleChangeOfInputFields = this.handleChangeOfInputFields.bind(this);
     this.handleResetButton = this.handleResetButton.bind(this);
-    this.handleSuccessModalOpen= this.handleSuccessModalOpen.bind(this);
-    this.handleSuccessModalClose= this.handleSuccessModalClose.bind(this);
-
+    this.handleSuccessModalOpen = this.handleSuccessModalOpen.bind(this);
+    this.handleSuccessModalClose = this.handleSuccessModalClose.bind(this);
   }
 
   handleResetButton() {
@@ -45,18 +40,17 @@ class EmailInputForReset extends React.Component {
         "content-type": "application/json"
       }
     }).then(response => {
-      this.handleSuccessModalOpen()
+      this.handleSuccessModalOpen();
     });
   }
 
-  handleSuccessModalOpen(){
-      this.setState({successModalOpen: true})
+  handleSuccessModalOpen() {
+    this.setState({ successModalOpen: true });
   }
 
-
-  handleSuccessModalClose(){
-    this.setState({successModalOpen: false})
-}
+  handleSuccessModalClose() {
+    this.setState({ successModalOpen: false });
+  }
 
   handleChangeOfInputFields(event) {
     event.preventDefault();
@@ -65,45 +59,61 @@ class EmailInputForReset extends React.Component {
 
   render() {
     return (
-      <div  style={{
-        maxWidth: "70%",
-        border: "1px solid green",
-        padding: "2px",
-        margin: "5% auto 5% auto",
-      }}>
-      <Form className="form">
-        <Col>
-          <FormGroup>
-            <Label for="examplePassword">Enter new password</Label>
-            <Input
-              type="password"
-              name="password"
-              id="password"
-              onChange={this.handleChangeOfInputFields}
-            />
-          </FormGroup>
-        </Col>
-        <MaterialUiLibrary.Button variant="contained" color="primary" onClick={this.handleResetButton}>RESET</MaterialUiLibrary.Button>
-
-      </Form>
-      <Dialog
-        open={this.state.successModalOpen}
-        onClose={this.handleSuccessModalClose}
-        aria-labelledby="alert-dialog-title"
-        aria-describedby="alert-dialog-description"
+      <div
+        style={{
+          maxWidth: "70%",
+          padding: "2px",
+          margin: "5% auto 5% auto"
+        }}
       >
-        <DialogTitle id="alert-dialog-title">{"Success"}</DialogTitle>
-        <DialogContent>
-          <DialogContentText id="alert-dialog-description">
-            Password has been reset successfully try logging in with the new password!
-          </DialogContentText>
-        </DialogContent>
-        <DialogActions>
-          <MaterialUiLibrary.Button onClick={this.handleSuccessModalClose} color="primary">
-            OK
-          </MaterialUiLibrary.Button>
-        </DialogActions>
-      </Dialog>
+        <HomeNavBar />
+        <Card style={{ backgroundColor: "#D8BFD8", marginTop: '2rem' }}>
+          <CardContent>
+            <Form className="form">
+              <Col>
+                <FormGroup>
+                  <Label for="examplePassword">Enter new password</Label>
+                  <Input
+                    type="password"
+                    name="password"
+                    id="password"
+                    onChange={this.handleChangeOfInputFields}
+                  />
+                </FormGroup>
+              </Col>
+              <MaterialUiLibrary.Button
+                variant="contained"
+                color="primary"
+                onClick={this.handleResetButton}
+              >
+                RESET
+              </MaterialUiLibrary.Button>
+            </Form>
+          </CardContent>
+        </Card>
+
+        <Dialog
+          open={this.state.successModalOpen}
+          onClose={this.handleSuccessModalClose}
+          aria-labelledby="alert-dialog-title"
+          aria-describedby="alert-dialog-description"
+        >
+          <DialogTitle id="alert-dialog-title">{"Success"}</DialogTitle>
+          <DialogContent>
+            <DialogContentText id="alert-dialog-description">
+              Password has been reset successfully try logging in with the new
+              password!
+            </DialogContentText>
+          </DialogContent>
+          <DialogActions>
+            <MaterialUiLibrary.Button
+              onClick={this.handleSuccessModalClose}
+              color="primary"
+            >
+              OK
+            </MaterialUiLibrary.Button>
+          </DialogActions>
+        </Dialog>
       </div>
     );
   }
