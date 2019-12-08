882e09370274e9b6636d730bca38d4b595664c1c
Sanskar <snskr95@gmail.com>
2019-12-08 15:22:37 +0530

    changes in email read sefvice to read attachemnts and upload to aws bucket

--- client/emailReadService.py
+++ client/emailReadService.py
@@ -1,15 +1,15 @@
-import datetime
-import smtplib
-import time
+
 import imaplib
 import email
 import email.utils
 from email.utils import parsedate_to_datetime
-# from client.service import getClientIdByClientEmailId
-
+from conversations.utils import uploadToAwsBucket
+import os
+from inclyfy import settings
 
 
 def read_email_from_gmail(projectInitiationDate):
+    global message
     try:
         mail = imaplib.IMAP4_SSL('imap.gmail.com')
         mail.login('collabere.outbox1@gmail.com', 'Collabere@2019')
@@ -30,22 +30,40 @@ def read_email_from_gmail(projectInitiationDate):
                     msg = email.message_from_string(response_part[1].decode("utf-8"))
                     email_subject = msg['subject']
                     email_from = msg['from']
-                    email_arrival_date=msg['Date']
-                    emailExistFlag= False
-                    collabereMessagesFilterFlag=email_subject.split()[len(email_subject.split())-1]=="Collabere"
+                    email_arrival_date = msg['Date']
+                    emailExistFlag = False
+                    collabereMessagesFilterFlag = email_subject.split()[len(email_subject.split()) - 1] == "Collabere"
                     if collabereMessagesFilterFlag:
-                       influencerUsername = email_subject.split()[3]
-                       clientEmailId=email_from.split()[len(email_from.split())-1].replace('>', '').replace('<', '')
-                       projectInitiationDateFromEmail=email_subject.split()[len(email_subject.split())-3]
-                       if msg.is_multipart():
-                         message=msg.get_payload()[0].get_payload().split('\r\n\r\n', 1)[0]
-                       if projectInitiationDateFromEmail==projectInitiationDate:
-                             return influencerUsername,clientEmailId,message,parsedate_to_datetime(email_arrival_date)
+                        influencerUsername = email_subject.split()[3]
+                        clientEmailId = email_from.split()[len(email_from.split()) - 1].replace('>', '').replace('<',
+                                                                                                                 '')
+                        projectInitiationDateFromEmail = email_subject.split()[len(email_subject.split()) - 3]
+                        if msg.is_multipart():
+                            for part in msg.walk():
+                                ctype = part.get_content_type()
+                                if ctype in {'image/jpeg', 'image/png', 'application/pdf',
+                                             'application/vnd.openxmlformats-officedocument.wordprocessingml.document'}:
+                                    try:
+                                        attachment = msg.get_payload()[1]
+                                        open(part.get_filename(), 'wb').write(attachment.get_payload(decode=True))
+                                        fileObject=open(part.get_filename(), 'r')
+                                        if projectInitiationDateFromEmail == projectInitiationDate:
+                                            uploadToAwsBucket(fileObject)
+                                            os.remove(part.get_filename())
+                                            fileUrlMessage=settings.FILE_URL_PREFIX+part.get_filename()
+                                            return influencerUsername, clientEmailId, fileUrlMessage, parsedate_to_datetime(
+                                                email_arrival_date)
+                                    except Exception as e:
+                                        print("Error occurred while uploading file to the bucket..")
+                            message = msg.get_payload()[0].get_payload().split('\r\n\r\n', 1)[0]
+                        if projectInitiationDateFromEmail == projectInitiationDate:
+                            return influencerUsername, clientEmailId, message, parsedate_to_datetime(email_arrival_date)
 
             return emailExistFlag
 
     except Exception as e:
         print(str(e))
+
 
 
 
