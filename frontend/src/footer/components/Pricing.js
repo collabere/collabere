ae61ee7e4f4baa292da24a0b3d1484698092b96a
Sanskar <snskr95@gmail.com>
2019-12-25 20:18:43 +0530

    alignment fix

--- frontend/src/footer/components/Pricing.js
+++ frontend/src/footer/components/Pricing.js
@@ -7,36 +7,38 @@ export default function PrivacyPolicy() {
     <Fragment>
       <HomeNavBar />
       <div
-        style={{ background: "#9577cb", padding: "2rem", marginTop: "3rem" }}
+        style={{ background: "#9577cb", padding: "2rem", marginTop: "2rem" }}
       >
-        <Row gutter={16}>
-          <Col span={8}>
+        <Row gutter={24}>
+          <Col span={12}>
             <Card title="Trial" bordered={true}>
               Validity: 1 month
               <h4 style={{ paddingTop: "2rem" }}>Free</h4>
             </Card>
           </Col>
-          <Col span={8}>
+          <Col span={12}>
             <Card title="Monthly" bordered={false}>
               Validity: 1 month
               <h4 style={{ paddingTop: "2rem" }}>Rs 350/-</h4>
             </Card>
           </Col>
-          <Col span={8}>
+        </Row>
+        <Row gutter={24} style={{ paddingTop: "4rem" }}>
+          <Col span={12}>
             <Card title="Quaterly" bordered={false}>
               Validity: 1 month
               <h4 style={{ paddingTop: "2rem" }}>Rs 1000/-</h4>
             </Card>
           </Col>
-        </Row>
-        <Row gutter={16} style={{ paddingTop: "4rem" }}>
-          <Col span={8}>
+          <Col span={12}>
             <Card title="Semi-Annual" bordered={false}>
               Validity: 1 month
               <h4 style={{ paddingTop: "2rem" }}>Rs 1800/-</h4>
             </Card>
           </Col>
-          <Col span={8}>
+        </Row>
+        <Row gutter={24} style={{ paddingTop: "4rem" }}>
+          <Col span={12}>
             <Card title="Annual" bordered={false}>
               Validity: 1 month
               <h4 style={{ paddingTop: "2rem" }}>Rs 3150/-</h4>
