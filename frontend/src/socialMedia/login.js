19bdd7c6f5d4c82d13f9a2bc3d441b19c711d866
nappingCommodore <niks.sbg@gmail.com>
2020-01-26 21:47:05 +0530

    Insta redirect URI corrected

--- frontend/src/socialMedia/login.js
+++ frontend/src/socialMedia/login.js
@@ -14,7 +14,7 @@ class Login extends React.Component {
       client_id: "49f4a71ef28b448a864a7519a197ba0c",
       client_secret: "db912bb77fd5472895e8e097191bb1a7",
       grant_type: "authorization_code",
-      redirect_uri: "http://localhost:8000/api/social_redirect",
+      redirect_uri: "http://www.collabere.com/api/social_redirect",
       code: response
     };
     axios
@@ -41,7 +41,7 @@ class Login extends React.Component {
         <InstagramLogin
           clientId="49f4a71ef28b448a864a7519a197ba0c"
           buttonText="Login With Instagram"
-          redirectUri="http://localhost:8000/api/social_redirect"
+          redirectUri="http://www.collabere.com/api/social_redirect"
           onSuccess={this.instagramResponse}
           onFailure={this.instagramResponse}
         />
