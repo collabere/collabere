3ba513d43131ce31c3fe9e5337cd7fa0a651b6f7
Sanskar <snskr95@gmail.com>
2020-04-05 16:33:46 +0530

    changed vieo on home page

--- frontend/src/homepage/components/HowItWorksSection.js
+++ frontend/src/homepage/components/HowItWorksSection.js
@@ -1,19 +1,19 @@
 import React from "react";
 import { Card, Typography, CardContent } from "@material-ui/core";
-import CollaborateImg from '../../../images/resized1.jpg';
+import CollaborateImg from "../../../images/resized1.jpg";
 
 export default function HowItWorksSection({ dark, id }) {
   const cardStyle = {
     minHeight: 400,
     minWidth: 275,
-    textAlign: 'center',
-    backgroundImage: `url(${CollaborateImg})`,
-  }
+    textAlign: "center",
+    backgroundImage: `url(${CollaborateImg})`
+  };
 
   const paddingStyle = {
     paddingBottom: 5,
-    paddingTop: 5,
-  }
+    paddingTop: 5
+  };
 
   return (
     <div style={paddingStyle}>
@@ -23,13 +23,14 @@ export default function HowItWorksSection({ dark, id }) {
             How It Works ?
           </Typography>
         </CardContent>
-        <iframe src='https://www.youtube.com/embed/h6fcK_fRYaI'
-          frameBorder='0'
-          allow='autoplay; encrypted-media'
+        <iframe
+          src="https://www.youtube.com/watch?v=PGJ284wFgmk"
+          frameBorder="0"
+          allow="autoplay; encrypted-media"
           allowFullScreen
-          title='video'
-          width='60%'
-          height='400'
+          title="video"
+          width="60%"
+          height="400"
         />
       </Card>
     </div>
